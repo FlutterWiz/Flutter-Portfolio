84ff83cdc042a7740d71947104507ab81205720b
Alper Efe Şahin <efe.mate@gmail.com>
2024-11-18 21:52:09 +0300

    made changes for featured projects

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 const Color white = Colors.white;
 const Color blackWithOpacity87 = Colors.black87;
 
-const Color black = Color.fromRGBO(31,41,55,1);
-const Color customOrange = Color.fromRGBO(241,68,68,1);
-const Color transitionWhite = Color.fromRGBO(248,249,252,1);
+const Color black = Color.fromRGBO(31, 41, 55, 1);
+const Color customOrange = Color.fromRGBO(241, 68, 68, 1);
+const Color transitionWhite = Color.fromRGBO(248, 249, 252, 1);
 const Color dividerColor = Color.fromRGBO(238, 238, 238, 1);
-const Color streamLightBlue =  Color.fromRGBO(239, 243, 255, 1.0);
-const Color streamDarkBlue =  Color.fromRGBO(0, 85, 255, 1.0);
-const Color streamDarkBlueText =  Color.fromRGBO(41, 70, 140, 1.0);
-
-
-
-
-
-
+const Color streamLightBlue = Color.fromRGBO(239, 243, 255, 1.0);
+const Color streamDarkBlue = Color.fromRGBO(0, 85, 255, 1.0);
+const Color streamDarkBlueText = Color.fromRGBO(41, 70, 140, 1.0);
+const Color aiLightOrange = Color.fromRGBO(255, 239, 237, 1);
+const Color aiDarkOrange = Color.fromRGBO(252, 76, 27, 1);
+const Color aiDarkOrangeText = Color.fromRGBO(196, 42, 0, 1);
--- lib/features/about/widgets/open_source/featured_projects.dart
+++ lib/features/about/widgets/open_source/featured_projects.dart
@@ -11,16 +11,74 @@ class FeaturedProjects extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Wrap(
+        spacing: 0,
+        alignment: WrapAlignment.spaceBetween,
+        children: [
+          ProjectContainer(
+            isMobile: isMobile,
+            gradientColors: const [streamLightBlue, white],
+            shadowColor: streamDarkBlue,
+            imagePath: "assets/stream.jpeg",
+            title: "Flutter Social Chat",
+            description:
+                "Hybrid, Functional, and Designed Chat App: Flutter Social Chat",
+            textColor: streamDarkBlueText,
+            isSponsored: true,
+          ),
+          ProjectContainer(
+            isMobile: isMobile,
+            isSponsored: false,
+            gradientColors: const [aiLightOrange, white],
+            shadowColor: aiDarkOrange,
+            imagePath: "assets/ai.png",
+            title: "Credit Card Fraud Detection",
+            description: "Detect Credit Card Frauds with Machine Learning",
+            textColor: aiDarkOrangeText,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProjectContainer extends StatelessWidget {
+  const ProjectContainer({
+    super.key,
+    required this.isMobile,
+    required this.gradientColors,
+    required this.shadowColor,
+    required this.imagePath,
+    required this.title,
+    required this.description,
+    required this.textColor,
+    required this.isSponsored,
+  });
+
+  final bool isMobile;
+  final bool isSponsored;
+  final List<Color> gradientColors;
+  final Color shadowColor;
+  final Color textColor;
+  final String imagePath;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       width: isMobile ? null : 550,
       height: 300,
       padding: const EdgeInsets.all(48),
+      margin: const EdgeInsets.only(bottom: 48),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
-        gradient: const LinearGradient(
+        gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: [streamLightBlue, white],
+          colors: gradientColors,
         ),
       ),
       child: Row(
@@ -31,83 +89,86 @@ class FeaturedProjects extends StatelessWidget {
             height: 50,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8),
-              image: const DecorationImage(
-                image: AssetImage("assets/stream.jpeg"),
+              color: black,
+              image: DecorationImage(
+                image: AssetImage(imagePath),
                 filterQuality: FilterQuality.high,
                 fit: BoxFit.fill,
               ),
-              boxShadow: const [
+              boxShadow: [
                 BoxShadow(
-                  color: streamDarkBlue,
-                  spreadRadius: 0,
-                  blurRadius: 15,
-                  offset: Offset(0, 5)
-                ),
+                    color: shadowColor,
+                    spreadRadius: 0,
+                    blurRadius: 15,
+                    offset: const Offset(0, 5)),
               ],
             ),
           ),
           const SizedBox(width: 24),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CustomText(
-                text: "Flutter Social Chat",
-                fontSize: 24,
-                height: 1.2,
-                fontWeight: FontWeight.w800,
-              ),
-              Container(
-                width: 350,
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: const CustomText(
-                  text:
-                      "Hybrid, Functional, and Designed Chat App: Flutter Social Chat",
-                  color: streamDarkBlueText,
-                  height: 1.6,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomText(
+                  text: title,
+                  fontSize: 24,
+                  height: 1.2,
+                  fontWeight: FontWeight.w800,
                 ),
-              ),
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        style: IconButton.styleFrom(
-                          backgroundColor: black,
-                          padding: const EdgeInsets.symmetric(horizontal: 32),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                        ),
-                        icon: const Row(
-                          children: [
-                            FaIcon(FontAwesomeIcons.github,
-                                color: white, size: 18),
-                            SizedBox(width: 8),
-                            CustomText(
-                              text: 'GitHub',
-                              color: white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ],
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: CustomText(
+                    text: description,
+                    color: textColor,
+                    height: 1.6,
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      style: IconButton.styleFrom(
+                        backgroundColor: black,
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
                         ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(width: 16),
-                  Text(
-                    "Sponsored by Stream!",
-                    style: GoogleFonts.robotoCondensed(
-                      fontSize: 20,
-                      decoration: TextDecoration.underline,
-                      decorationColor: black,
-                      decorationThickness: 1.5,
+                      icon: const Row(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.github,
+                            color: white,
+                            size: 18,
+                          ),
+                          SizedBox(width: 8),
+                          CustomText(
+                            text: 'GitHub',
+                            color: white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    isSponsored
+                        ? Padding(
+                            padding: const EdgeInsets.only(left: 12),
+                            child: Text(
+                              "Sponsored!",
+                              style: GoogleFonts.robotoCondensed(
+                                fontSize: 20,
+                                decoration: TextDecoration.underline,
+                                decorationColor: black,
+                                decorationThickness: 1.5,
+                              ),
+                            ),
+                          )
+                        : const SizedBox(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
