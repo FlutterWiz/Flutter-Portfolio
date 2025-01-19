9665aeed040b875cb712130582d0b89342b0aa24
Alper Efe Şahin <efe.mate@gmail.com>
2025-01-19 16:36:20 +0300

    implement setup of l10n

--- lib/core/init/app_widget.dart
+++ lib/core/init/app_widget.dart
@@ -1,6 +1,7 @@
 import 'package:alperefesahin_dev/core/constants/colors.dart';
 import 'package:alperefesahin_dev/presentation/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -10,11 +11,10 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'alperefesahin',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: blue),
-      ),
+      theme: ThemeData(useMaterial3: true, colorScheme: ColorScheme.fromSeed(seedColor: blue)),
       home: const HomePage(),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
